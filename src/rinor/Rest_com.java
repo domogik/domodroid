91a95f0963a09fbe89a4091bd053d5353f0978d0
tikismoke <webmaster@tikijs.dyndns.org>
2013-11-30 16:32:44 +0100

    Change restconnection acces to allow httpauth. Not working for the moment.

--- src/rinor/Rest_com.java
+++ src/rinor/Rest_com.java
@@ -21,22 +21,28 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.net.SocketException;
-
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.conn.HttpHostConnectException;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.CoreConnectionPNames;
 import org.json.JSONException;
 import org.json.JSONObject;
 
- 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import misc.tracerengine;
+
+  
 public class Rest_com {
- 
-    private static String convertStreamToString(InputStream is) {
+	private SharedPreferences params;
+	
+	
+	private static String convertStreamToString(InputStream is) {
         BufferedReader reader = new BufferedReader(new InputStreamReader(is));
         StringBuilder sb = new StringBuilder();
  
@@ -57,45 +63,54 @@ public class Rest_com {
         return sb.toString();
     }
  
-    public static JSONObject connect(String url) throws Exception
+    @SuppressWarnings("null")
+	public static JSONObject connect(String url)
     {
-    	String message = "???";
+    	//String login = params.getString("http_auth_username",null);
+    	//String password = params.getString("http_auth_password",null);
+    	String login ="";
+    	String password ="";
+    	
+    	tracerengine Tracer = null;
     	JSONObject json = null;
-        HttpClient httpclient = new DefaultHttpClient();
-        HttpGet httpget = new HttpGet(url); 
-        HttpResponse response;
-        String result = null;
-        try {
+    		try {
+    		DefaultHttpClient httpclient = new DefaultHttpClient();
+    		httpclient.getCredentialsProvider().setCredentials( new AuthScope(null, -1),new UsernamePasswordCredentials(login+":"+password));
+    		// TODO Set timeout
+    		// this doesn't work
+    		//httpclient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 10000);
+    		HttpGet httpget = new HttpGet(url);    		
+    		HttpResponse response;
+    		String result = null;
             response = httpclient.execute(httpget);
-            HttpEntity entity = response.getEntity();
-            if (entity != null) {
-                InputStream instream = entity.getContent();
-                result= convertStreamToString(instream);
-                json=new JSONObject(result);
-                instream.close();
-                return json;
-            }
+            if(response.getStatusLine().getStatusCode() == 200)
+			{
+            	HttpEntity entity = response.getEntity();
+			    if (entity != null) {
+			    	InputStream instream = entity.getContent();
+			    	result= convertStreamToString(instream);
+			    	json=new JSONObject(result);
+			    	instream.close();
+			    }
+			}
+            else
+			{
+            	Tracer.d("Rest_com", "Resource not available>");
+			}
  
  
         } catch (HttpHostConnectException e) {
-        	message = " HttpHostConnectException";
-        	//e.printStackTrace();
-        } catch (SocketException e) {
-        	message = " SocketException";
         	//e.printStackTrace();
         } catch (ClientProtocolException e) {
-        	message = " ClientProtocolException";
-        	//e.printStackTrace();
+        	e.printStackTrace();
         } catch (IOException e) {
-        	message = " IOException";
-        	//e.printStackTrace();
+        	e.printStackTrace();
         } catch (JSONException e) {
-        	message = " JSONException";
-        	//e.printStackTrace();
+        	e.printStackTrace();
         }
-        Exception e = new Exception("Rest error :"+message);
-        throw e;
-        
+        return json;
     }
- 
+	public void setParams(SharedPreferences params) {
+		this.params = params;
+	}
 }

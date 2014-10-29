b13bfde1a1fff3526551b63b8bd7910f4a08bfa1
tikismoke <webmaster@tikijs.dyndns.org>
2014-10-29 13:56:27 +0100

    remove unused method mailto!

--- src/widgets/Basic_Graphical_widget.java
+++ src/widgets/Basic_Graphical_widget.java
@@ -39,7 +39,6 @@ import android.content.DialogInterface;
 import android.content.pm.FeatureInfo;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.net.MailTo;
 import android.view.Gravity;
 import android.view.View;
 import android.view.View.OnLongClickListener;

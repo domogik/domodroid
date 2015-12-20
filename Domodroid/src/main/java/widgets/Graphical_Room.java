23d7553382aee8cff40b9a6ed25e5ec327fae01e
tikismoke <webmaster@tikijs.dyndns.org>
2015-12-20 20:45:35 +0100

    #38 Don't translate device name/ area/room

--- Domodroid/src/main/java/widgets/Graphical_Room.java
+++ Domodroid/src/main/java/widgets/Graphical_Room.java
@@ -18,6 +18,7 @@
 package widgets;
 
 import org.domogik.domodroid13.R;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -25,155 +26,153 @@ import activities.Graphics_Manager;
 
 import database.Cache_management;
 import database.DmdContentProvider;
+
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Handler;
+
 import misc.List_Icon_Adapter;
 import misc.tracerengine;
+
 import android.view.View;
 import android.view.View.OnLongClickListener;
 import android.widget.EditText;
 import android.widget.FrameLayout;
 import android.widget.ListView;
 
-public class Graphical_Room extends Basic_Graphical_zone implements OnLongClickListener{
+public class Graphical_Room extends Basic_Graphical_zone implements OnLongClickListener {
 
-	public final FrameLayout container = null;
-	public FrameLayout myself = null;
-	private final Context context;
-	private final int id_room;
-	private tracerengine Tracer = null;
-	private String mytag="Graphical_Room";
-	private String icon;
-	private final Activity Activity;
+    public final FrameLayout container = null;
+    public FrameLayout myself = null;
+    private final Context context;
+    private final int id_room;
+    private tracerengine Tracer = null;
+    private String mytag = "Graphical_Room";
+    private String icon;
+    private final Activity Activity;
 
-	public Graphical_Room(tracerengine Trac, Context context,int id,String name_room, String description_room, String icon, int widgetSize, Handler handler) {
-		super(context, id, name_room, description_room, icon, widgetSize, "room", handler);
-		this.myself = this;
-		this.Tracer = Trac;
-		this.id_room = id;
-		this.context = context;
-		this.icon=icon;
-		this.Activity = (android.app.Activity) context;
-		setOnLongClickListener(this);
-		mytag="Graphical_Room("+id_room+")";		
-	}
+    public Graphical_Room(tracerengine Trac, Context context, int id, String name_room, String description_room, String icon, int widgetSize, Handler handler) {
+        super(context, id, name_room, description_room, icon, widgetSize, "room", handler);
+        this.myself = this;
+        this.Tracer = Trac;
+        this.id_room = id;
+        this.context = context;
+        this.icon = icon;
+        this.Activity = (android.app.Activity) context;
+        setOnLongClickListener(this);
+        mytag = "Graphical_Room(" + id_room + ")";
+    }
 
 
-	public boolean onLongClick(View v) {
-		final AlertDialog.Builder list_type_choice = new AlertDialog.Builder(getContext());
-		List<String> list_choice = new ArrayList<String>();
-		list_choice.add(context.getString(R.string.change_icon));
-		list_choice.add(context.getString(R.string.rename));
-		list_choice.add(context.getString(R.string.delete));
-		final CharSequence[] char_list =list_choice.toArray(new String[list_choice.size()]);
-		//list_type_choice.setTitle(R.string.What_to_do_message);
-		list_type_choice.setSingleChoiceItems(char_list, -1,
-				new DialogInterface.OnClickListener() {
-			public void onClick(DialogInterface dialog, int item) {
-				ListView lw = ((AlertDialog)dialog).getListView();
-				Object checkedItem = lw.getAdapter().getItem(lw.getCheckedItemPosition());
-				do_action(checkedItem.toString());
-				dialog.cancel();
-			}
-		}
-				);
+    public boolean onLongClick(View v) {
+        final AlertDialog.Builder list_type_choice = new AlertDialog.Builder(getContext());
+        List<String> list_choice = new ArrayList<String>();
+        list_choice.add(context.getString(R.string.change_icon));
+        list_choice.add(context.getString(R.string.rename));
+        list_choice.add(context.getString(R.string.delete));
+        final CharSequence[] char_list = list_choice.toArray(new String[list_choice.size()]);
+        //list_type_choice.setTitle(R.string.What_to_do_message);
+        list_type_choice.setSingleChoiceItems(char_list, -1,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int item) {
+                        ListView lw = ((AlertDialog) dialog).getListView();
+                        Object checkedItem = lw.getAdapter().getItem(lw.getCheckedItemPosition());
+                        do_action(checkedItem.toString());
+                        dialog.cancel();
+                    }
+                }
+        );
 
-		list_type_choice.show();
-		return false;
-	}
+        list_type_choice.show();
+        return false;
+    }
 
-	private void do_action(String action) {
-		if(action.equals(context.getString(R.string.delete))) {
-			AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
-			alert.setTitle(context.getString(R.string.Delete_feature_title)+" "+name);
-			alert.setMessage(R.string.Delete_feature_message);
-			alert.setPositiveButton(R.string.reloadOK, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog_customname, int whichButton) {
-					Tracer.get_engine().remove_one_things(id_room,"room");
-					Tracer.get_engine().remove_one_place_type_in_Featureassociation(id_room,"room");
-					Tracer.get_engine().remove_one_icon(id_room,"room");
-					//recheck cache element to remove those no more need.
-					Cache_management.checkcache(Tracer,Activity);
-					removeView(LL_background);
-					myself.setVisibility(GONE);
-					if(container != null) {
-						container.removeView(myself);
-						container.recomputeViewAttributes(myself);
-					}
-				}
-			});
-			alert.setNegativeButton(R.string.reloadNO, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog_customname, int whichButton) {
-					Tracer.e(mytag, "delete Canceled.");
-				}
-			});
-			alert.show();
-		}else if (action.equals(context.getString(R.string.rename))){
-			AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
-			alert.setTitle(context.getString(R.string.Rename_title)+" "+name);
-			alert.setMessage(R.string.Rename_message);
-			// Set an EditText view to get user input 
-			final EditText input = new EditText(getContext());
-			alert.setView(input);
-			alert.setPositiveButton(R.string.reloadOK, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog_customname, int whichButton) {
-					String result= input.getText().toString(); 
-					Tracer.get_engine().descUpdate(id_room, result, "room");
-					try{
-						TV_name.setText(context.getResources().getString(Graphics_Manager.getStringIdentifier(getContext(), result.toLowerCase())).toString());
-					}catch (Exception e){
-						Tracer.d(mytag, "no translation for: "+result);
-						TV_name.setText(result);
-					}
-				}
-			});
-			alert.setNegativeButton(R.string.reloadNO, new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog_customname, int whichButton) {
-					Tracer.e(mytag, "Customname Canceled.");
-				}
-			});
-			alert.show();
-		}else if (action.equals(context.getString(R.string.change_icon))){
-			final AlertDialog.Builder list_icon_choice = new AlertDialog.Builder(getContext());
-			List<String> list_icon = new ArrayList<String>();
-			String[] fiilliste;
-			fiilliste = context.getResources().getStringArray(R.array.icon_area_array); 
-			for (int i=0; i < fiilliste.length ; i++){
-				list_icon.add(fiilliste[i]);
-			}
-			final CharSequence[] char_list_icon =list_icon.toArray(new String[list_icon.size()]);
-			list_icon_choice.setTitle(context.getString(R.string.Wich_ICON_message)+" "+name);
-			List_Icon_Adapter adapter=new List_Icon_Adapter(getContext(), fiilliste);
-			list_icon_choice.setAdapter(adapter,null );
-			list_icon_choice.setSingleChoiceItems(char_list_icon, -1,
-					new DialogInterface.OnClickListener() {
-				public void onClick(DialogInterface dialog, int item) {
-					ListView lw = ((AlertDialog)dialog).getListView();
-					Object checkedItem = lw.getAdapter().getItem(lw.getCheckedItemPosition());
-					icon = checkedItem.toString();
-					ContentValues values = new ContentValues();
-					//type = area, room, feature
-					values.put("name", "room");
-					//icon is the name of the icon wich will be select 
-					values.put("value", icon);
-					//reference is the id of the area, room, or feature
-					int reference = 0;
-					reference=id_room;
-					values.put("reference", reference);
-					context.getContentResolver().insert(DmdContentProvider.CONTENT_URI_UPDATE_ICON_NAME, values);
-					IV_img.setBackgroundResource(Graphics_Manager.Icones_Agent(icon, 0));
-					dialog.cancel();
-				}
-			}
-					);	
-			AlertDialog alert_list_icon = list_icon_choice.create();
-			alert_list_icon.show();
+    private void do_action(String action) {
+        if (action.equals(context.getString(R.string.delete))) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+            alert.setTitle(context.getString(R.string.Delete_feature_title) + " " + name);
+            alert.setMessage(R.string.Delete_feature_message);
+            alert.setPositiveButton(R.string.reloadOK, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog_customname, int whichButton) {
+                    Tracer.get_engine().remove_one_things(id_room, "room");
+                    Tracer.get_engine().remove_one_place_type_in_Featureassociation(id_room, "room");
+                    Tracer.get_engine().remove_one_icon(id_room, "room");
+                    //recheck cache element to remove those no more need.
+                    Cache_management.checkcache(Tracer, Activity);
+                    removeView(LL_background);
+                    myself.setVisibility(GONE);
+                    if (container != null) {
+                        container.removeView(myself);
+                        container.recomputeViewAttributes(myself);
+                    }
+                }
+            });
+            alert.setNegativeButton(R.string.reloadNO, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog_customname, int whichButton) {
+                    Tracer.e(mytag, "delete Canceled.");
+                }
+            });
+            alert.show();
+        } else if (action.equals(context.getString(R.string.rename))) {
+            AlertDialog.Builder alert = new AlertDialog.Builder(getContext());
+            alert.setTitle(context.getString(R.string.Rename_title) + " " + name);
+            alert.setMessage(R.string.Rename_message);
+            // Set an EditText view to get user input
+            final EditText input = new EditText(getContext());
+            alert.setView(input);
+            alert.setPositiveButton(R.string.reloadOK, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog_customname, int whichButton) {
+                    String result = input.getText().toString();
+                    Tracer.get_engine().descUpdate(id_room, result, "room");
+                    TV_name.setText(result);
+                }
+            });
+            alert.setNegativeButton(R.string.reloadNO, new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog_customname, int whichButton) {
+                    Tracer.e(mytag, "Customname Canceled.");
+                }
+            });
+            alert.show();
+        } else if (action.equals(context.getString(R.string.change_icon))) {
+            final AlertDialog.Builder list_icon_choice = new AlertDialog.Builder(getContext());
+            List<String> list_icon = new ArrayList<String>();
+            String[] fiilliste;
+            fiilliste = context.getResources().getStringArray(R.array.icon_area_array);
+            for (int i = 0; i < fiilliste.length; i++) {
+                list_icon.add(fiilliste[i]);
+            }
+            final CharSequence[] char_list_icon = list_icon.toArray(new String[list_icon.size()]);
+            list_icon_choice.setTitle(context.getString(R.string.Wich_ICON_message) + " " + name);
+            List_Icon_Adapter adapter = new List_Icon_Adapter(getContext(), fiilliste);
+            list_icon_choice.setAdapter(adapter, null);
+            list_icon_choice.setSingleChoiceItems(char_list_icon, -1,
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int item) {
+                            ListView lw = ((AlertDialog) dialog).getListView();
+                            Object checkedItem = lw.getAdapter().getItem(lw.getCheckedItemPosition());
+                            icon = checkedItem.toString();
+                            ContentValues values = new ContentValues();
+                            //type = area, room, feature
+                            values.put("name", "room");
+                            //icon is the name of the icon wich will be select
+                            values.put("value", icon);
+                            //reference is the id of the area, room, or feature
+                            int reference = 0;
+                            reference = id_room;
+                            values.put("reference", reference);
+                            context.getContentResolver().insert(DmdContentProvider.CONTENT_URI_UPDATE_ICON_NAME, values);
+                            IV_img.setBackgroundResource(Graphics_Manager.Icones_Agent(icon, 0));
+                            dialog.cancel();
+                        }
+                    }
+            );
+            AlertDialog alert_list_icon = list_icon_choice.create();
+            alert_list_icon.show();
 
-		}	
-	}
+        }
+    }
 
 }

0eb9338baed326e713991d63bdda58c0b69577cd
tikismoke <webmaster@tikijs.dyndns.org>
2016-06-06 22:25:24 +0200

    Update libs to last version with a test error to validate test.

---
+++ Domodroid/src/test/java/Entity/Entity_AreaTest.java
@@ -0,0 +1,39 @@
+package Entity;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by tiki on 06/06/2016.
+ */
+public class Entity_AreaTest {
+
+    @Test
+    public void testSetGetDescription() throws Exception {
+        Entity_Area area = new Entity_Area(null, null, null, null, 0, null);
+        area.setDescription("Description");
+        String description = area.getDescription();
+        Assert.assertEquals("Description", description);
+    }
+
+    @Test
+    public void testSetGetId() throws Exception {
+        Entity_Area area = new Entity_Area(null, null, null, null, 0, null);
+        area.setId(125);
+        Integer id = area.getId();
+        Assert.assertEquals(125, id, 0);
+    }
+
+    @Test
+    public void testSetGetName() throws Exception {
+        Entity_Area area = new Entity_Area(null, null, null, null, 0, null);
+        area.setName("Name");
+        String name = area.getName();
+        Assert.assertEquals("Names", name);
+    }
+
+    @Test
+    public void testGetIcon_name() throws Exception {
+        //// TODO: 06/06/2016
+    }
+}

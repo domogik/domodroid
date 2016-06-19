761a777915142a982efd15b8c40661efb4edc9f5
tikismoke <webmaster@tikijs.dyndns.org>
2016-06-19 16:11:22 +0200

    Display color in sensor widget for DT_ColorRGB and send command correctly as list red,green,blue

--- Domodroid/src/main/java/misc/Color_Result.java
+++ Domodroid/src/main/java/misc/Color_Result.java
@@ -5,12 +5,14 @@ import android.content.Context;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.util.Log;
 import android.view.View;
 
 public class Color_Result extends View {
     public float[] hsvCurrent = {1, 1, 1};
     private final Paint mPaint;
-    public String color;
+    public String color = null;
+    public String colorrgb = null;
 
     public Color_Result(Context context) {
         super(context);
@@ -19,12 +21,22 @@ public class Color_Result extends View {
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if (color == null) {
-            mPaint.setColor(Color.HSVToColor(hsvCurrent));
-        } else {
-            mPaint.setColor(Color.parseColor(color));
+        try {
+            if (color == null && colorrgb == null) {
+                mPaint.setColor(Color.HSVToColor(hsvCurrent));
+            } else if (color == null && colorrgb != null) {
+                String[] parsecolor = colorrgb.split(",");
+                int red = Integer.parseInt(parsecolor[0]);
+                int green = Integer.parseInt(parsecolor[1]);
+                int blue = Integer.parseInt(parsecolor[2]);
+                mPaint.setColor(Color.rgb(red, green, blue));
+            } else if (color != null && colorrgb == null) {
+                mPaint.setColor(Color.parseColor(color));
+            }
+            canvas.drawRect(0, 10, 100, 100, mPaint);
+        } catch (Exception e) {
+            Log.e("", e.toString());
         }
-        canvas.drawRect(0, 10, 100, 100, mPaint);
     }
 
     @Override

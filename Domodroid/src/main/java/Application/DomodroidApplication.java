057313d7426c4ce6af91605db802b4faf5cf599d
tikismoke <webmaster@tikijs.dyndns.org>
2016-10-08 12:07:53 +0200

    Close #118 Adding Acra crash report systems

---
+++ Domodroid/src/main/java/Application/DomodroidApplication.java
@@ -0,0 +1,28 @@
+package Application;
+
+import android.app.Application;
+
+import org.acra.ACRA;
+import org.acra.ReportingInteractionMode;
+import org.acra.annotation.ReportsCrashes;
+import org.domogik.domodroid13.R;
+
+/**
+ * Created by tiki on 07/10/2016.
+ */
+
+
+@ReportsCrashes(formUri = "http://yourserver.com/yourscript",
+        mailTo = "reports@yourdomain.com",
+        mode = ReportingInteractionMode.TOAST,
+        resToastText = R.string.toast_crash)
+
+public class DomodroidApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // The following line triggers the initialization of ACRA
+        ACRA.init(this);
+    }
+}

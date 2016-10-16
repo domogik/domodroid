7dc60c9bc2ca63f1415acf8f8255bf9ae56d6ed1
tikismoke <webmaster@tikijs.dyndns.org>
2016-10-16 12:24:27 +0200

    Acra mail devs and information corrections

--- Domodroid/src/main/java/applications/domodroid.java
+++ Domodroid/src/main/java/applications/domodroid.java
@@ -7,13 +7,20 @@ import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 import org.domogik.domodroid13.R;
 
+import static org.acra.ReportField.ANDROID_VERSION;
+import static org.acra.ReportField.CUSTOM_DATA;
+import static org.acra.ReportField.LOGCAT;
+import static org.acra.ReportField.PHONE_MODEL;
+import static org.acra.ReportField.STACK_TRACE;
+
 /**
  * Created by tiki on 07/10/2016.
  */
 
 
 @ReportsCrashes(formUri = "http://yourserver.com/yourscript",
-        mailTo = "reports@yourdomain.com",
+        mailTo = "new.domodroid@gmail.com",
+        customReportContent = { ANDROID_VERSION, PHONE_MODEL, STACK_TRACE, LOGCAT },
         mode = ReportingInteractionMode.TOAST,
         resToastText = R.string.toast_crash)
 

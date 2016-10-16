ca24ba5e224fcca5d1886488642d7b71817105a5
tikismoke <webmaster@tikijs.dyndns.org>
2016-10-16 13:13:16 +0200

    Close #120 Add ANR watchdog

--- Domodroid/src/main/java/applications/domodroid.java
+++ Domodroid/src/main/java/applications/domodroid.java
@@ -2,13 +2,14 @@ package applications;
 
 import android.app.Application;
 
+import com.github.anrwatchdog.ANRWatchDog;
+
 import org.acra.ACRA;
 import org.acra.ReportingInteractionMode;
 import org.acra.annotation.ReportsCrashes;
 import org.domogik.domodroid13.R;
 
 import static org.acra.ReportField.ANDROID_VERSION;
-import static org.acra.ReportField.CUSTOM_DATA;
 import static org.acra.ReportField.LOGCAT;
 import static org.acra.ReportField.PHONE_MODEL;
 import static org.acra.ReportField.STACK_TRACE;
@@ -20,7 +21,7 @@ import static org.acra.ReportField.STACK_TRACE;
 
 @ReportsCrashes(formUri = "http://yourserver.com/yourscript",
         mailTo = "new.domodroid@gmail.com",
-        customReportContent = { ANDROID_VERSION, PHONE_MODEL, STACK_TRACE, LOGCAT },
+        customReportContent = {ANDROID_VERSION, PHONE_MODEL, STACK_TRACE, LOGCAT},
         mode = ReportingInteractionMode.TOAST,
         resToastText = R.string.toast_crash)
 
@@ -31,5 +32,6 @@ public class domodroid extends Application {
         super.onCreate();
         // The following line triggers the initialization of ACRA
         ACRA.init(this);
+        new ANRWatchDog().start();
     }
 }

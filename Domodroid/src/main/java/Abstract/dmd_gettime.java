8e93992a2460078bb521e9854b9bce08bf5776c0
tikismoke <webmaster@tikijs.dyndns.org>
2016-11-14 19:40:16 +0100

    Method to convert time in abstract

---
+++ Domodroid/src/main/java/Abstract/dmd_gettime.java
@@ -0,0 +1,46 @@
+package Abstract;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+/**
+ * Created by tiki on 14/11/2016.
+ */
+
+public class dmd_gettime {
+    static final String DATEFORMAT = "yyyy-MM-dd HH:mm:ss";
+
+    public static Date GetUTCdatetimeAsDate()
+    {
+        //note: doesn't check for null
+        return StringDateToDate(GetUTCdatetimeAsString());
+    }
+
+    public static String GetUTCdatetimeAsString()
+    {
+        final SimpleDateFormat sdf = new SimpleDateFormat(DATEFORMAT);
+        sdf.setTimeZone(TimeZone.getTimeZone("UTC"));
+        final String utcTime = sdf.format(new Date());
+
+        return utcTime;
+    }
+
+    public static Date StringDateToDate(String StrDate)
+    {
+        Date dateToReturn = null;
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATEFORMAT);
+
+        try
+        {
+            dateToReturn = (Date)dateFormat.parse(StrDate);
+        }
+        catch (ParseException e)
+        {
+            e.printStackTrace();
+        }
+
+        return dateToReturn;
+    }
+}

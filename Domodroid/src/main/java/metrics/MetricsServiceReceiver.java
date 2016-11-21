9738c58af644ca1f0a8d4c4e8ca89a74e479ff8b
tikismoke <webmaster@tikijs.dyndns.org>
2016-11-21 23:05:04 +0100

    #122 Prepare json to send to metrics server

--- Domodroid/src/main/java/metrics/MetricsServiceReceiver.java
+++ Domodroid/src/main/java/metrics/MetricsServiceReceiver.java
@@ -11,14 +11,21 @@ import android.preference.PreferenceManager;
 import android.provider.Settings;
 import android.util.Log;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
 import org.domogik.domodroid13.BuildConfig;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.text.DecimalFormat;
-
-import Abstract.dmd_gettime;
+import java.util.Date;
 
 import static activities.Activity_Main.context;
 
@@ -27,13 +34,16 @@ import static activities.Activity_Main.context;
  */
 
 public class MetricsServiceReceiver extends BroadcastReceiver {
-    private final String mytag = this.getClass().getName();
+    private static final String mytag = "MetricsServiceReceiver";
     ActivityManager.MemoryInfo mi = new ActivityManager.MemoryInfo();
     Float freeSize = 0f;
     Float totalAllocatedSize = 0f;
     Float usedSize = -1f;
     Float maxmemavailable = 0f;
     DecimalFormat df = new DecimalFormat("#.##");
+    JSONObject measurements = new JSONObject();
+    JSONObject tags = new JSONObject();
+    static JSONObject metrics = new JSONObject();
 
     @Override
     public void onReceive(Context context, Intent intent) {
@@ -43,34 +53,73 @@ public class MetricsServiceReceiver extends BroadcastReceiver {
     private class getmetrics extends AsyncTask<Void, Void, Void> {
         @Override
         protected Void doInBackground(Void... params) {
-            //process with update timer for new 30 sec
-            Log.e(mytag, "New timer " + executeTop());
+            try {
+                tags.put("component", "domodroid");
+                tags.put("interface", "yes");
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             SharedPreferences SP_params = PreferenceManager.getDefaultSharedPreferences(context);
             Float api_version = SP_params.getFloat("API_VERSION", 0);
-            Log.e(mytag, "New timer api_version:" + api_version.toString());
+            try {
+                tags.put("domogik_api_version", api_version.toString());
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             String domogik_version = SP_params.getString("DOMOGIK-VERSION", "");
-            Log.e(mytag, "New timer domogik_version:" + domogik_version);
+            try {
+                tags.put("domogik_version", domogik_version);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             String versionName = BuildConfig.VERSION_NAME;
-            Log.e(mytag, "New timer versionName:" + versionName);
+            try {
+                tags.put("domodroid_version_name", versionName);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             int versionCode = BuildConfig.VERSION_CODE;
             String vcs = "??";
             if (versionCode != -1)
                 vcs = Integer.toString(versionCode);
-            Log.e(mytag, "New timer VERSION_CODE:" + vcs);
+            try {
+                tags.put("domodroid_version_code", vcs);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             int Android_code = Build.VERSION.SDK_INT;
-            Log.e(mytag, "New timer SDK_INT:" + String.valueOf(Android_code));
+            try {
+                tags.put("domodroid_sdk", String.valueOf(Android_code));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
             String Device = Build.DEVICE;
-            Log.e(mytag, "New timer Device:" + Device);
-            String devicid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
-            Log.e(mytag, "New timer devicid:" + devicid);
+            try {
+                tags.put("domodroid_device", Device);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            String deviceid = Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+            try {
+                metrics.put("id", deviceid);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            //process with update timer for new 30 sec
+            Log.e(mytag, "New timer " + executeTop());
             try {
                 Runtime info = Runtime.getRuntime();
                 int availableProcessors = info.availableProcessors();
-                Log.e(mytag, "New timer availableProcessors: " + availableProcessors);
+                try {
+                    tags.put("num_core", availableProcessors);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
                 maxmemavailable = Float.valueOf(info.maxMemory());
                 maxmemavailable = maxmemavailable / 1024; //in KB
                 maxmemavailable = maxmemavailable / 1024; //in MB
-                Log.e(mytag, "New timer maxmemavailable: " + df.format(maxmemavailable) + "MB");
+                measurements.put("memory_total", df.format(maxmemavailable));
+                measurements.put("unit", 1);
                 freeSize = Float.valueOf(info.freeMemory());
                 totalAllocatedSize = Float.valueOf(info.totalMemory());
                 usedSize = totalAllocatedSize - freeSize;
@@ -83,13 +132,72 @@ public class MetricsServiceReceiver extends BroadcastReceiver {
                 Log.e(mytag, "New timer totalAllocatedSize: " + df.format(totalAllocatedSize) + "MB");
                 Log.e(mytag, "New timer freeSize: " + df.format(freeSize) + "MB");
                 Log.e(mytag, "New timer usedSize: " + df.format(usedSize) + "MB");
+            } catch (JSONException e) {
+                e.printStackTrace();
             } catch (Exception e) {
                 Log.e(mytag, "error getting used memory :" + e.toString());
             }
-            String gmtTime = dmd_gettime.GetUTCdatetimeAsString();
-            Log.e(mytag, "time stamp=" + gmtTime);
+            long millis = new Date().getTime();
+            String gmtTime = String.valueOf(millis);
+            gmtTime = gmtTime.substring(0, gmtTime.length() - 3) + "." + gmtTime.substring(gmtTime.length() - 3, gmtTime.length());
+            try {
+                metrics.put("tags", tags);
+                metrics.put("timestamp", gmtTime);
+                metrics.put("measurements", measurements);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.e(mytag, "metrics=" + metrics.toString());
+
+            //Try to send metrics to domogik metrics server
+            //String result = POST("http://metrics.domogik.org/metrics/");
+            //Log.e(mytag, "result=" + result);
+
             return null;
         }
+    }
+
+    public static String POST(String url) {
+        InputStream inputStream = null;
+        String result = "";
+        try {
+            HttpClient httpclient = new DefaultHttpClient();
+            HttpPost httpPost = new HttpPost(url);
+            StringEntity se = new StringEntity(metrics.toString());
+            httpPost.setEntity(se);
+
+            httpPost.setHeader("accept", "json");
+            httpPost.setHeader("Content-type", "application/json");
+
+            HttpResponse httpResponse = httpclient.execute(httpPost);
+
+            inputStream = httpResponse.getEntity().getContent();
+
+            // convert inputstream to string
+            if (inputStream != null) {
+                result = convertInputStreamToString(inputStream);
+                Log.d(mytag, "inputStream result" + result);
+            } else
+                result = "Did not work!";
+            Log.d(mytag, "result" + result);
+
+        } catch (Exception e) {
+            Log.d(mytag, "e.getLocalizedMessage()" + e.getLocalizedMessage());
+        }
+
+        //  return result
+        return result;
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null)
+            result += line;
+        inputStream.close();
+        return result;
+
     }
 
     private String executeTop() {
